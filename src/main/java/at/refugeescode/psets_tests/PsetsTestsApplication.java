32541762950f438bfbbeff826dba61304feaa360
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-21 16:52:22 +0100

    basic start

---
+++ src/main/java/at/refugeescode/psets_tests/PsetsTestsApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.psets_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsetsTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsetsTestsApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/psets_tests/PsetsTestsApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.psets_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsetsTestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
